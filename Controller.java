a0657354708a5709da8a380b9cd0bae62d323d67
Ramil <ramilyusupov228@gmail.com>
2018-11-20 15:03:13 +0500

    исправления

--- Controller.java
+++ Controller.java
@@ -46,6 +46,22 @@ private void calcIntegral(){
                 sum += integral.calculate(x);
             });
 
+    /*
+    если интеграл двойной, то
+    public static double doubleIntMonteCarlo(double a, double b, double c, double d, int n)
+{
+     double sum = 0D, xSum = 0D, ySum = 0D;
+
+     for (int i = 0; i < n; i++)
+     {
+           xSum = a + Math.random() * Math.abs(b - a);
+           ySum = c + Math.random() * Math.abs(d - c);
+           sum += f(xSum, ySum);
+     }
+     return Math.abs(b - a) * Math.abs(d - c) * sum / n;
+}
+     */
+
     //вводим оценку в textfield
     double assesmentMonteKarlo = (a - b)* sum / iteration;
     assesment.appendText(String.format("%.3f", assesmentMonteKarlo));
